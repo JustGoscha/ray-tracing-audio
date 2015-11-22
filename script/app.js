c259d972d9aef27a13871ee06affffc585179384
Georg Graf <justgoscha@gmail.com>
2015-11-23 03:13:15 +0400

    Initial commit with simple ray line intersection working

--- script/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-console.log("Everything is fine! For now... application starts [Static Dependencies loaded!]");
-
-angular.module('app', ['ngResource']);
--- script/bootstrap.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-console.log("Bootstrapping of app: Start...");
-angular.bootstrap(document, ['app']);
-console.log("Bootstrapping of app: DONE!");
