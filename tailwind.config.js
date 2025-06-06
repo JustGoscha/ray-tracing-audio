a59060dc8eda1690777b00d246c271a295188d71
Georg Graf <justgoscha@MacBookPro.box>
2025-06-06 23:16:01 +0200

    Tutorial / how to and links

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./js/**/*.{js,ts}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
